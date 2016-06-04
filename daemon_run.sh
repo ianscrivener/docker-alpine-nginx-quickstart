fef0e6791138e2d3e398d807f376c98244d51822
Ian Scrivener <ian.scrivener@cabcharge.com.au>
2016-06-04 19:44:17 +1000

    upd

--- daemon_run.sh
+++ daemon_run.sh
@@ -2,8 +2,7 @@
 eval "$(weave env)"
 docker run -d \
     -p 8080:8080 \
-    --privileged=true \
-    -v /root/data/nginx/config/:/etc/nginx/:ro \
+    -v /root/data/nginx:/etc/nginx:ro \
     -v /root/data/nginx/html:/usr/share/nginx/html:ro \
     --name=nginx \
     nginx:alpine
