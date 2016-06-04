fda0106a9ad503f299797f8baa932ee82d63a3e1
Ian Scrivener <ian.scrivener@cabcharge.com.au>
2016-06-04 17:50:56 +1000

    upd

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 rm -rf /root/data/nginx
 mkdir -p /root/data/nginx/html
 printf "admin:$(openssl passwd -crypt fudge888)n" > /root/data/nginx/passwords
-mkdir -p /root/data/nginx/config/conf.d
-cp nginx.conf /root/data/nginx/
+#mkdir -p /root/data/nginx/config/conf.d
+cp nginx.conf /root/data/nginx/nginx.conf
 
 #docker run -d --name tmp-nginx nginx:alpine
 #docker cp tmp-nginx:/etc/nginx/nginx.conf /root/data/nginx/config/nginx.conf
