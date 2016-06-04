a971e044683d09c640e4bdaaf7051796eda6c600
Ian Scrivener <ian.scrivener@cabcharge.com.au>
2016-06-04 17:05:38 +1000

    upd

--- daemon_run.sh
+++ daemon_run.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 docker run -d \
+    -p 8080:8080 \
+    --privileged=true \
+    -v /root/data/nginx/config/:/etc/nginx/:ro \
+    -v /root/data/nginx/html:/usr/share/nginx/html:ro \
+    --name=nginx \
+    nginx:alpine
--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+rm -rf /root/data/nginx
 mkdir -p /root/data/nginx/html
 mkdir -p /root/data/nginx/auth
 printf "admin:$(openssl passwd -crypt fudge888)n" > /root/data/nginx/auth/passwords
 mkdir -p /root/data/nginx/config/conf.d
 cp nginx.conf /root/data/nginx/config/
+
 #docker run -d --name tmp-nginx nginx:alpine
 #docker cp tmp-nginx:/etc/nginx/nginx.conf /root/data/nginx/config/nginx.conf
 #docker stop tmp-nginx
