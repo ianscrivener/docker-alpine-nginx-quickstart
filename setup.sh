dd7993de33ae278f58b706c12e116cfe743a44a1
Ian Scrivener <ian.scrivener@cabcharge.com.au>
2016-06-04 17:53:56 +1000

    upd

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rm -rf /root/data/nginx
 mkdir -p /root/data/nginx/html
-printf "admin:$(openssl passwd -crypt fudge888)n" > /root/data/nginx/passwords
+printf "admin:$apr1$koTeN7qG$g4VHt9kxmamjs6MYVejiB/" > /root/data/nginx/passwords
 #mkdir -p /root/data/nginx/config/conf.d
 cp nginx.conf /root/data/nginx/nginx.conf
 
